09cd56e7e26e9a2208dedbd65ce09458ab3a5e57
Sunday Brian <b.sunday@platcorpgroup.com>
2022-10-14 09:00:17 +0300

    setup intial base class

---
+++ src/base.ts
@@ -0,0 +1,44 @@
+import { Config } from "./types";
+import axios, { Axios } from "axios"
+
+
+
+export abstract class Base {
+    private username: string;
+    private baseUrl: string;
+    private password: string;
+    private client: Axios;
+
+
+    constructor(config: Config) {
+        this.username = config.username;
+        this.password = config.password;
+        this.baseUrl = config.baseUrl ?? "https://cweb.dnbtanzania.com/TanWebsite/Services/LiveRequestService.svc?wsdl";
+
+        this.client = axios.create({
+            baseURL: this.baseUrl,
+            headers: {
+                "Content-Type": "text/xml; charset=utf-8",
+                Accept: "*/*",
+                SOAPAction: "http://tempuri.org/ILiveRequestService/GetLiveCIR"
+            }
+        })
+    }
+
+
+    protected invoke<T>(
+        data: any
+    ): Promise<T> {
+        // const config
+        return this.client.post("", data).then(response => {
+            return response.data
+        }).catch(err => { throw err })
+
+
+    }
+
+
+
+
+
+}
